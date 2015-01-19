7a4e54ae473bf11d1f46d7cdc146e86d7186a975
Ilton Garcia dos Santos Silveira <ilton_unb@hotmail.com>
2015-01-19 09:20:47 -0200

    base iOS project created

---
+++ PictureCard/PictureCard/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PictureCard
+//
+//  Created by Ilton  Garcia on 19/01/15.
+//  Copyright (c) 2015 EstudosLivres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PictureCard/PictureCard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureCard
+//
+//  Created by Ilton  Garcia on 19/01/15.
+//  Copyright (c) 2015 EstudosLivres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
